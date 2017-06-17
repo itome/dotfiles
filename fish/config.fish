9314b28db33491bf0be1ed9b35c2522b16f332b2
itome <t.t.itm.0403@gmail.com>
2017-06-17 21:25:33 +0900

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,23 @@ set fish_greeting ""
 function fish_title
   true
 end
+
+# incremental search with peco and z
+function peco_z
+  set -l query (commandline)
+
+  if test -n $query
+    set peco_flags --query "$query"
+  end
+
+  z -l | peco $peco_flags | awk '{ print $2 }' | read recent
+  if [ $recent ]
+    cd $recent
+    commandline -r ''
+    commandline -f repaint
+  end
+end
+
+function fish_user_key_bindings
+  bind \x1b peco_z # bind to C-[
+end
