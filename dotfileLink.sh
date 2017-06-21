0d246424f053a5ca1db0d1811d226b1dc72c1f33
itome <t.t.itm.0403@gmail.com>
2017-06-21 13:42:50 +0900

    fish shell support

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -2,3 +2,6 @@
 ln -sf ~/dotfiles/.spacemacs ~/.spacemacs
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/fish ~/.config/
+
+## to support fish shell in emacs
+tic -o ~/.terminfo $TERMINFO/e/eterm-color.ti
