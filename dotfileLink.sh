d52f7aa066c444a7ddcc8c70b00b769a92cccdbe
itome <t.t.itm.0403@gmail.com>
2017-08-10 16:46:38 +0900

    Add .tigrc

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.jsbeautifyrc ~/.jsbeautifyrc
 ln -sf ~/dotfiles/.tern-config ~/.tern-config
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
 
 ## to support fish shell in emacs
 tic -o ~/.terminfo $TERMINFO/e/eterm-color.ti
