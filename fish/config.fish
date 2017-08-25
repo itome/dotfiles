9cb1a563bf17fa30c167c04af956c5a0e6fd722b
itome <t.t.itm.0403@gmail.com>
2017-08-25 22:57:21 +0900

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -23,3 +23,9 @@ function fish_user_key_bindings
 end
 
 alias weather="wget http://wttr.in -O -"
+
+set -g theme_display_date no
+set -g theme_title_use_abbreviated_path no
+set -g theme_show_exit_status yes
+set -g theme_color_scheme gruvbox
+set -g fish_prompt_pwd_dir_length 0
