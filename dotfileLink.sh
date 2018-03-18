d4e6e62159c0b15cff5cca91338de01b044c4921
itome <t.t.itm.0403@gmail.com>
2018-03-18 13:17:09 +0900

    Add .jsbeautifyrc

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.spacemacs.d/init.el ~/.spacemacs
 # original layers for spacemacs
-ln -s ~/dotfiles/.spacemacs.d/+itome/ ~/.emacs.d/layers/+itome/
+ln -s ~/dotfiles/.spacemacs.d/+itome ~/.emacs.d/layers/+itome
 
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/fish ~/.config/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.jsbeautifyrc ~/.jsbeautifyrc
 ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tern-config ~/.tern-config
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
