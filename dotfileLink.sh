d7c560ad3410c6e85891dcb4b401fa4b6b3c23c1
itome <t.t.itm.0403@gmail.com>
2018-03-18 13:02:21 +0900

    Add editor config support

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -6,7 +6,6 @@ ln -s ~/dotfiles/.spacemacs.d/+itome/ ~/.emacs.d/layers/+itome/
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/fish ~/.config/
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.jsbeautifyrc ~/.jsbeautifyrc
 ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tern-config ~/.tern-config
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
