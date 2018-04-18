d9e5d76173cf91a1821aaf7e2047ee0fb58928e4
takeshi-tsukamoto <t.t.itm.0403@gmail.com>
2018-04-18 15:44:47 +0900

    Update fish dotfile

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,13 @@
 # remove greeting message
 set fish_greeting ""
 
-# get along with emacs
-function fish_title
-  true
-end
-
-# emacs ansi-term support
-if test -n "$EMACS"
-  set -x TERM eterm-color
-end
-
-# make peco bottom up
-function peco
-  command peco --layout=bottom-up $argv
-end
-
 # key bindings
 function fish_user_key_bindings
   bind \cx\cf peco_z
-  bind \cr 'peco_select_history (commandline -b)'
 end
 
+alias peco="peco --layout=bottom-up"
+alias grep="grep --color"
 alias weather="wget http://wttr.in -O -"
+alias gs "git status"
+alias gd "git diff"
