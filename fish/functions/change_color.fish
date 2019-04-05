fa076fffaac661c06d4a71e39379414d829c28d5
itome <itometeam@gmail.com>
2019-04-05 13:32:19 +0900

    Remove unused

--- fish/functions/change_color.fish
+++
@@ -1,6 +0,0 @@
-# rename files recursively
-# usage: _rename <file_name> <color>
-
-function change_color
-  find . -name $argv[1] | xargs -ICON convert CON -fuzz 90% -channel RGBA -fill $argv[2] -opaque \#212121 CON
-end
