1034473f5c0a8e1024b1f21a4ed2f2cdee71ac05
itome <itometeam@gmail.com>
2019-05-22 21:34:52 +0900

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -6,5 +6,14 @@ ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/karabiner ~/.config/karabiner
 ln -sf ~/dotfiles/peco-config.json ~/.config/peco/config.json
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/xkeysnail/xkeysnail.desktop ~/.config/autostart/xkeysnail.desktop
+
+if [ "$(uname)" == 'Darwin' ]; then
+    ln -sf ~/dotfiles/.Brewfile ~/.Brewfile
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    ln -sf ~/dotfiles/xkeysnail/xkeysnail.desktop ~/.config/autostart/xkeysnail.desktop
+else
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+fi
+
 chmod +x ~/dotfiles/commands/*
