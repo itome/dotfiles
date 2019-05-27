c18be6db1ce34791c2752b153bcb5d3993974665
itome <itometeam@gmail.com>
2019-05-27 12:57:10 +0900

    Update dotfiles

--- install-mac.sh
+++ install-mac.sh
@@ -5,7 +5,7 @@ xcode-select --install
 if !(type "brew" > /dev/null 2>&1); then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-brew bundle --file ~/dotfiles/Brewfile
+brew bundle --global
 
 # rust
 if !(type "cargo" > /dev/null 2>&1); then
