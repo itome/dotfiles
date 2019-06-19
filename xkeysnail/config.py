d30ab3473f74b7878af6b13c8a503c246bc15eb8
Takeshi Tsukamoto <itometeam@gmail.com>
2019-06-20 01:58:55 +0900

    Update xkeysnail config

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -8,25 +8,3 @@ define_multipurpose_modmap({
     Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
     Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
 })
-
-
-# emacs-like keybindings in non-emacs applications
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
-    # Cursor
-    K("C-b"): with_mark(K("left")),
-    K("C-f"): with_mark(K("right")),
-    K("C-p"): with_mark(K("up")),
-    K("C-n"): with_mark(K("down")),
-    K("C-h"): with_mark(K("backspace")),
-    # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
-    # Newline
-    K("C-m"): K("enter"),
-    # Undo
-    K("C-slash"): [K("C-z"), set_mark(False)],
-    K("C-Shift-ro"): K("C-z")
-}, "Emacs-like keys")
