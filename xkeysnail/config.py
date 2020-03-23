20927e0cc150d4aa882d2d79ffe1ccaf46a50c8f
Takeshi Tsukamoto <itometeam@gmail.com>
2020-03-24 00:02:01 +0900

    Update xkeysnail

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -6,13 +6,18 @@ from xkeysnail.transform import *
 define_multipurpose_modmap({
     Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
     Key.RIGHT_CTRL: [Key.ESC, Key.RIGHT_CTRL],
+    Key.LEFT_CTRL: [Key.ESC, Key.LEFT_CTRL],
     Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
     Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
 })
 
-
 # emacs-like keybindings in non-emacs applications
 define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
     # Newline
-    K("C-m"): K("enter")
+    K("C-m"): K("enter"),
+    # Newline
+    K("C-f"): K("right"),
+    K("C-b"): K("left"),
+    K("C-p"): K("up"),
+    K("C-n"): K("down"),
 }, "Emacs-like keys")
