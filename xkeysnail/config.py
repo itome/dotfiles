3d13425426ebce18bb6390aef8ae9d1d6a88adab
Takeshi Tsukamoto <itometeam@gmail.com>
2020-03-24 13:27:26 +0900

    Fix bug

--- xkeysnail/config.py
+++ xkeysnail/config.py
@@ -3,21 +3,27 @@
 import re
 from xkeysnail.transform import *
 
-define_multipurpose_modmap({
-    Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
-    Key.RIGHT_CTRL: [Key.ESC, Key.RIGHT_CTRL],
-    Key.LEFT_CTRL: [Key.ESC, Key.LEFT_CTRL],
-    Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
-    Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT]
-})
+define_multipurpose_modmap(
+    {
+        Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL],
+        Key.RIGHT_CTRL: [Key.ESC, Key.RIGHT_CTRL],
+        Key.LEFT_CTRL: [Key.ESC, Key.LEFT_CTRL],
+        Key.LEFT_ALT: [Key.MUHENKAN, Key.LEFT_ALT],
+        Key.RIGHT_ALT: [Key.HENKAN, Key.RIGHT_ALT],
+    }
+)
 
 # emacs-like keybindings in non-emacs applications
-define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt"), {
-    # Newline
-    K("C-m"): K("enter"),
-    # Newline
-    K("C-f"): K("right"),
-    K("C-b"): K("left"),
-    K("C-p"): K("up"),
-    K("C-n"): K("down"),
-}, "Emacs-like keys")
+define_keymap(
+    lambda wm_class: wm_class not in ("Emacs", "URxvt", "Emacs-snapshot"),
+    {
+        # Newline
+        K("C-m"): K("enter"),
+        # Newline
+        K("C-f"): K("right"),
+        K("C-b"): K("left"),
+        K("C-p"): K("up"),
+        K("C-n"): K("down"),
+    },
+    "Emacs-like keys",
+)
