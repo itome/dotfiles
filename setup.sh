5ee50a45f1d45ea866df1ce236c742d831b04377
Takeshi Tsukamoto <itometeam@gmail.com>
2020-04-23 08:46:56 -0700

    Update dotfiles

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/karabiner
 ln -sf ~/dotfiles/.profile ~/.profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.xvimrc ~/.xvimrc
 ln -sf ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-ln -sf ~/dotfiles/peco-config.json ~/.config/peco/config.json
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
