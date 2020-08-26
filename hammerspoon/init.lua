1dfec1be114e9174a84ff6e339c6ba9f3721111b
Takeshi Tsukamoto <itometeam@gmail.com>
2020-08-27 04:27:43 +0900

    Update hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,7 @@
 hs.loadSpoon('ControlEscape'):start()
 
-hs.hotkey.bind({"ctrl"}, "M", function()
-    hs.eventtap.keyStroke({}, 'return', 0)
-end)
+hs.hotkey.bind({"ctrl"}, "M",
+  function() hs.eventtap.keyStroke({}, 'return', 0) end,
+  nil,
+  function() hs.eventtap.keyStroke({}, 'return', 0) end
+)
