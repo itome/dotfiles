20820cd1ddb0ddcb68bc62bb50323c2da7edf3ca
Takeshi Tsukamoto <dev@itome.team>
2020-09-04 12:22:46 +0900

    Update hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,3 +6,21 @@ hs.hotkey.bind({"ctrl"}, "M",
   nil,
   function() hs.eventtap.keyStroke({}, 'return', 0) end
 )
+
+hs.hotkey.bind({"ctrl"}, "P",
+  function() hs.eventtap.keyStroke({}, 'up', 0) end,
+  nil,
+  function() hs.eventtap.keyStroke({}, 'up', 0) end
+)
+
+hs.hotkey.bind({"ctrl"}, "N",
+  function() hs.eventtap.keyStroke({}, 'down', 0) end,
+  nil,
+  function() hs.eventtap.keyStroke({}, 'down', 0) end
+)
+
+hs.hotkey.bind({"ctrl"}, "I",
+  function() hs.eventtap.keyStroke({}, 'tab', 0) end,
+  nil,
+  function() hs.eventtap.keyStroke({}, 'tab', 0) end
+)
